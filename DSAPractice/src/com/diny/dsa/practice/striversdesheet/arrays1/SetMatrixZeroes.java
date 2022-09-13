89339a77f79f2a0a63c360510104b8de8f772e78
Dinesh Patnaik <dinpatna@in.ibm.com>
2022-09-14 01:06:39 +0530

    Initial boilerplate code commit for SetMatrixZeroes

---
+++ DSAPractice/src/com/diny/dsa/practice/striversdesheet/arrays1/SetMatrixZeroes.java
@@ -0,0 +1,11 @@
+package com.diny.dsa.practice.striversdesheet.arrays1;
+
+class SetMatrixZeroes {
+	public void setZeroes(int[][] matrix) {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
